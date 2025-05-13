a435c49bbb963c7e9fd6976dd649c8ba3f6ea502
JEONGMUN LEE <lot9616@gmail.com>
2025-05-14 00:41:38 +0900

    Create generator.go

---
+++ utils/generator.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomString(length int) string {
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(result)
+}
